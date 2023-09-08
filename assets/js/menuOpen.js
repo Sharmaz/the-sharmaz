c110e500366d7b42490488abe90a1b4480e8e009
Ivan Robles <irae45@gmail.com>
2023-09-07 23:09:59 -0600

    add header menu styles for mobile.

--- assets/js/menuOpen.js
+++ assets/js/menuOpen.js
@@ -1,7 +1,20 @@
 // Toggle the menu open and close when on mobile
 export default function menuOpen() {
-    const burgerButton = document.querySelector('.gh-burger');
-    burgerButton.addEventListener('click', function () {
-        document.body.classList.toggle('gh-head-open');
-    });
-}
+  const burgerButton = document.querySelector('.burger-button');
+  const burgerElements = document.querySelectorAll('.burger-element');
+  const menu = document.querySelector('.gh-head-menu');
+  const menuActions = document.querySelector('.gh-head-actions');
+  burgerButton.addEventListener('click', function () {
+    burgerElements[0].classList.toggle('-translate-y-3');
+    burgerElements[0].classList.toggle('rotate-[40deg]');
+    burgerElements[1].classList.toggle('opacity-0');
+    burgerElements[2].classList.toggle('translate-y-3');
+    burgerElements[2].classList.toggle('-rotate-[40deg]');
+    menu.classList.toggle('hidden');
+    menuActions.classList.toggle('hidden');
+    menuActions.classList.toggle('flex');
+    menu.classList.toggle('flex');
+    menu.classList.toggle('backdrop-blur');
+    document.body.classList.toggle('overflow-hidden');
+  });
+};
